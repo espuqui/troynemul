cb313dcbffb7a9a73880d3c4cb2c5f645044c55d
Marcos Fuentes <ffuentesmarcos@gmail.com>
2025-03-02 17:01:11 -0800

    Tooltips

--- render.js
+++ render.js
@@ -1,6 +1,21 @@
 window.addEventListener('load', function () {
   render("mew")
+
+  handleTooltips()
 });
+
+function handleTooltips() {
+  document.addEventListener('click', function(event) {
+    let elements = document.getElementsByClassName("tooltip")
+    for (let elem of elements) {
+      if (elem === event.target) {
+        elem.children[0].style.visibility = 'visible'
+      } else {
+        elem.children[0].style.visibility = 'hidden'
+      }
+    }
+  });
+}
 
 
 function render() {
@@ -19,11 +34,10 @@ function render() {
 
   exampleList.innerHTML = renderMapu() + "<br/>" + renderWinka("lo amenacé con un cuchillo") + "<br /><br />"
   exampleList.innerHTML += renderMapu() + "<br/>" + renderWinka("lo amenacé con un cuchillo")
-
 }
 
 function renderWithSpan(text, spanClass) {
-  return '<span class="' + spanClass + '">' + text + '</span>'
+  return `<span class="${spanClass}">${text}</span>`
 }
 
 function renderWithSpanOnClickAlert(text, spanClass) {
@@ -31,19 +45,23 @@ function renderWithSpanOnClickAlert(text, spanClass) {
 }
 
 function renderWithSpanOnClick(text, spanClass, code) {
-  return '<span class="' + spanClass + '" onclick="' + code + '">' + text + '</span>'
+  return `<span class="${spanClass}" onclick="${code}">${text}</span>`
+}
+
+function renderWithSpanOnClickTooltip(text, tooltipText, spanClass) {
+    return `<span class="tooltip spanClass">${text}<span>${tooltipText}</span></span>`
 }
 
 function renderMapu() {
   let html = '<img src="img/mapuche_flag.svg" width="20px" height="20px" alt="">'
   html += " "
-  html += renderWithSpanOnClickAlert("anel", "normalWordExample")
+  html += renderWithSpanOnClickTooltip("anel", "amenazar", "normalWordExample")
   html += renderWithSpan("tu", "particleExample1")
   html += renderWithSpan("fiñ", "particleExample2")
   html += " "
-  html += renderWithSpan("kiñe", "normalWordExample")
+  html += renderWithSpanOnClickTooltip("kiñe", "amenazar", "normalWordExample")
   html += " "
-  html += renderWithSpan("kuchillo", "normalWordExample")
+  html += renderWithSpanOnClickTooltip("kuchillo", "amenazar", "normalWordExample")
   html += " "
   html += renderWithSpan("mew", "particleExample3 mainParticle")
 
