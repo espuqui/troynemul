089a1d907bd2d4b51856b5f69a1c67787e9f5409
Marcos Fuentes <ffuentesmarcos@gmail.com>
2025-07-05 22:52:28 -0700

    Contacto + explicacion de grupalizadores

--- app/js/render.js
+++ app/js/render.js
@@ -4,7 +4,7 @@ import {search, loadSearchEvent} from "./search.js";
 import {UIStatus, Views} from "./uistatus.js";
 import {updateUI} from "./updateui.js";
 
-const startWord = "fe|nom"
+const startWord = "engün|com"
 
 
 /**
